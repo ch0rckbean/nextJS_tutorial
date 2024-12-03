35148c093d3a7e3b89c638474d7049787d9001d8
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-12-03 21:19:03 +0900

    study: page & layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>대시보드 레이아웃 {children}</>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardPage() {
+  return <>DASHBOARD 페이지</>;
+}
